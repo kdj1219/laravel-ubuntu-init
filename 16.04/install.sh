dc4c2f6bfdef92b76eaddeae52402c4881f51981
Daniel Kong <kongdejia@gmail.com>
2018-09-20 13:22:14 -0700

    add yarn back

--- 16.04/install.sh
+++ 16.04/install.sh
@@ -32,6 +32,9 @@ function init_repositories {
     add-apt-repository -y ppa:ondrej/php
     add-apt-repository -y ppa:nginx/stable
 
+    curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+    echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
     apt-get update
 }
 
