08b07ee90ba267e7013678b9b2afd8d2072a8593
Daniel Kong <kongdejia@gmail.com>
2018-10-01 12:38:12 -0700

    安装 elasticsearch

--- 16.04/install_elasticsearch.sh
+++ 16.04/install_elasticsearch.sh
@@ -12,6 +12,10 @@ function install_java {
 function install_es {
     curl -sS https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
     
+    sudo apt-get install apt-transport-https
+    
+    echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
+    
     apt-get update
     apt-get install -y elasticsearch
     systemctl enable elasticsearch.service
